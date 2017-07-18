52626404f2eaef4d4b7cb1900dcb9daa3b98b0cd
David Zheng <davidzheng@knights.ucf.edu>
2017-07-18 17:30:36 -0400

    design things

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :email, length: { minimum: 5 }
-  validates :password, length: { minimum: 6 }
+  # validates :password, length: { minimum: 6 }
+
   def full_name
     "#{fname} #{lname}".strip
   end
